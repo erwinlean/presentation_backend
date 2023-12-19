1f77ec0fa869db87cfc5785dac8b65e9ec122f91
erwin <erwin.mdq@gmail.com>
2023-12-19 22:17:16 +0100

    dumb

--- utils/emailStyles.js
+++ utils/emailStyles.js
@@ -1,5 +1,6 @@
 /**
  * @param {string} mensaje
+ * 
  * @param {string} nombre
  * @returns {string}
 */
@@ -10,7 +11,6 @@ function htmlResetEmail(nombre, mensaje) {
     // Style added directly as HTML
     return `
         <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; text-align: center; color:black;">
-            <img src="${logo}" style="width: 100px; height: auto; margin-bottom: 20px;" alt='Eco-encuentro logo' title="Eco-encuentro Logo"/>
             <h1 style="color: #61AE4E;">Mensaje de: ${nombre}</h1>
             <br>
             <p style="font-size: 18px; margin-bottom: 20px;">${mensaje}</p>
