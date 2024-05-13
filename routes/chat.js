8559f1d155459c0e07bc0ed0fea6a613f49606d3
Erwin Marte <85772547+erwinlean@users.noreply.github.com>
2024-05-13 21:05:22 +0200

    NT

--- routes/chat.js
+++ routes/chat.js
@@ -4,12 +4,11 @@ var express = require('express');
 var router = express.Router();
 const chatController = require("../controllers/chatController");
 const authenticateToken = require("../middleware/verifyToken");
-const {apiLimiter} = require("../middleware/security");
+const { apiLimiter } = require("../middleware/security");
 
 // Save msg to the DB
-router.get('/', authenticateToken,apiLimiter ,chatController.getAllChat);
-router.post('/',authenticateToken,apiLimiter , chatController.addNewChat);
-router.delete('/delete', authenticateToken,apiLimiter, chatController.deleteAllChats);
-
+router.get('/', authenticateToken, apiLimiter, chatController.getAllChat);
+router.post('/', authenticateToken, apiLimiter, chatController.addNewChat);
+router.delete('/delete', authenticateToken, apiLimiter, chatController.deleteAllChats);
 
 module.exports = router;
